479ac3b3c2f7436f83b6c2bdeee6e3d26d8ac19f
”Krupanu” <Krupanu2@gmail.com>
2024-07-20 17:33:11 +0300

    creation

---
+++ Anton/src/Birthday.java
@@ -0,0 +1,8 @@
+
+public class Birthday
+{
+    public static void main(String args[])  {
+        Message message=new Message();
+        message.wish();
+    }
+}
---
+++ Anton/src/Message.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Message
+{
+    void wish() {
+        var scanner = new Scanner(System.in);
+        System.out.println("\n\n                                                                                                                                           ^      ^      ^");
+        System.out.println("HHHH     HHHH       AA         PPPPPPPPPPP  PPPPPPPPPPP    YY         YY                                                                   |      |      |");
+        System.out.println("HHHH     HHHH      AAAA        PP       PP  PP       PP     YY       YY                                                                    |      |      |");
+        System.out.println("HHHH     HHHH     AA  AA       PP       PP  PP       PP      YY     YY                                                                    ~~~~~~~~~~~~~~~~~   ");
+        System.out.println("HHHHHHHHHHHHH    AA    AA      PPPPPPPPPPP  PPPPPPPPPPP       YYYYYYY                                                                    |                 |");
+        System.out.println("HHHH     HHHH   AAAAAAAAAA     PP           PP                    YY                                                                     |                 |");
+        System.out.println("HHHH     HHHH  AA        AA    PP           PP                   YY                                                                  ____|                 |___");
+        System.out.println("HHHH     HHHH AA          AA   PP           PP                  YY                                                                   |     ``````````````      |");
+        System.out.println("                                                                                                                                     |                         |");
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHHH       HHHH  DDDDDDDDDDD        AA   YY          YY                      |     HAPPY BIRTH DAY     |");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D      AAAA   YY        YY                 ______|                         |_____");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D    AA  AA   YY      YY                  |      `````````````````````````      |");
+        System.out.println("BBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHHHH  DD          D   AA    AA   YYYYYYYY                   |                                     |");
+        System.out.println("BB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D  AAAAAAAAAA       YY                    |_____________________________________|");
+        System.out.println("BB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D  AA        AA     YY  ");
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHHH       HHHH  DDDDDDDDDDD  AA          AA   YY ");
+        System.out.println("\n\nOpen up BirthDay.txt file for more)");
+
+    }
+}
